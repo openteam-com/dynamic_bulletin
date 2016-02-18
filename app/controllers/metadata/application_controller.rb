230f4bf2050d62f15544acf4e0382c4ba0c86a1a
PeoneEr <peoneerko@yandex.ru>
2016-02-18 15:10:58 +0600

    added rescue from cancan

--- app/controllers/metadata/application_controller.rb
+++ app/controllers/metadata/application_controller.rb
@@ -1,5 +1,6 @@
 class Metadata::ApplicationController < ApplicationController
   load_and_authorize_resource
+
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
@@ -8,5 +9,9 @@ class Metadata::ApplicationController < ApplicationController
 
   def current_ability
     Ability.new(current_user, :metadata)
+  end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
   end
 end
