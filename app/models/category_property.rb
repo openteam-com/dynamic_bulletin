350767a92db991b426f3d425e39056bc935ad6dd
Sergey Petrushin <systemofadown.2013@yandex.ru>
2016-03-24 13:40:44 +0700

    Default value for limited_list

--- app/models/category_property.rb
+++ app/models/category_property.rb
@@ -2,14 +2,24 @@ class CategoryProperty < ActiveRecord::Base
   include RankedModel
   ranks :row_order, with_same: :category_id
 
-  scope :by_position, -> { order('row_order') }
-  default_scope { by_position }
-
   belongs_to :category
   belongs_to :property
-  extend Enumerize
-  if self.column_names.include? 'show_as'
-    enumerize :show_as, in: [:check_boxes, :radio_buttons, :select]
-  end
+
+  scope :by_position, -> { order('row_order') }
+  default_scope { by_position }
 end
 
+# == Schema Information
+#
+# Table name: category_properties
+#
+#  id             :integer          not null, primary key
+#  category_id    :integer
+#  property_id    :integer
+#  created_at     :datetime
+#  updated_at     :datetime
+#  show_on_public :boolean          default(TRUE)
+#  necessarily    :boolean          default(FALSE)
+#  show_as        :string           default("check_boxes")
+#  row_order      :integer
+#
