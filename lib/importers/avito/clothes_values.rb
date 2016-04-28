e651311918faa8b48d973360698e71109b076954
SERGO <you@example.com>
2016-04-28 13:41:40 +0600

    поправлен импортер для Вещи

--- lib/importers/avito/clothes_values.rb
+++ lib/importers/avito/clothes_values.rb
@@ -12,23 +12,24 @@ module Avito
           end
           find_size = property.list_items.where('title ilike ?', "%#{size}%").first
           if self_category.title == "Для мужчин"
-            property.values.create list_item_id: find_size.id, advert_id: advert.id
+            property.values.create list_item_id: find_size.id, advert_id: advert.id if !find_size.nil?
           else
-            property.values.create list_item_ids: find_size.id, advert_id: advert.id
+            property.values.create list_item_ids: find_size.id, advert_id: advert.id if !find_size.nil?
           end
         end
         property = self_category.properties.where('title ilike ?', "%предмет%").first
         adv["params"][1]["value"] = "Футболки" if adv["params"][1]["value"] == "Трикотаж и футболки"
+        adv["params"][1]["value"] = "Нижнее белье" if adv["params"][1]["value"] == "Нижнее бельё"
         find_value = property.list_items.where('title ilike ?', "%#{adv["params"][1]["value"]}%").first
         if self_category.title == "Для мужчин"
-          property.values.create list_item_id: find_value.id, advert_id: advert.id
+          property.values.create list_item_id: find_value.id, advert_id: advert.id if !find_value.nil?
         else
-          property.values.create list_item_ids: find_value.id, advert_id: advert.id
+          property.values.create list_item_ids: find_value.id, advert_id: advert.id if !find_value.nil?
         end
       elsif self_category.parent.title == "Обувь"
         property = self_category.properties.where('title ilike ?', "%размер%").first
         find_value = property.list_items.where('title ilike ?', "%#{adv["params"][2]["value"]}%").first
-        property.values.create list_item_id:  find_value.id, advert_id: advert.id
+        property.values.create list_item_id:  find_value.id, advert_id: advert.id if !find_value.nil?
       end
     end
   end
